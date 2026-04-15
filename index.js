c8e5fc91148d5b24f78f6dd4d4dcd87b94aeb2c6
Mehakpreetkaur87 <mehakpreet.kaur@radpretation.ai>
2026-04-15 18:19:37 +0530

    Added products.js file and used /p in the index.js file now the url becomes http://localhost:3000/p/ and use it app.use('/p', products) for products

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ const port = 3000;
 // import item router file
 const item  = require('./routes/item');
 
+
+// add router file of the products
+const products = require('./routes/products.js')
+
 // load into application
 app.use('/api', item);
+
+// need to use the products routers 
+
+app.use('/p', products)
+
 
 //  - /api/ -> item home page
 //  - /api/items -> item post request
